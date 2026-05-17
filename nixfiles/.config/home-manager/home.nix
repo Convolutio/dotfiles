69af89dbb3aa906d2cf78b26e37d82c5f09266f5
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-05-17 21:27:24 +0200

    fix: working wezterm sourcing + zsh

--- nixfiles/.config/home-manager/home.nix
+++ nixfiles/.config/home-manager/home.nix
@@ -70,10 +70,38 @@
   #
   home.sessionVariables = {
     EDITOR = "nvim";
+    TERMINAL = "wezterm";
+  };
+  home.sessionPath = [
+    "$HOME/bin"
+    "$HOME/.local/bin"
+  ];
+  # Let Home Manager manage the shell
+  # Zsh
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+  };
+  # Starship for cool zsh
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
 
   # Imports
   imports = [ lazyvim.homeManagerModules.default ];
